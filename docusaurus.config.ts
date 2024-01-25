a456e7124eea692f86149ca0e9740c1732eabd41
shinjith-dev <shinjithpallikkara@gmail.com>
2024-01-25 23:11:59 +0530

    feat: Add faq

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,125 +1,137 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: 'SpotX',
-  tagline: 'A Multi-Purpose Ad-Blocker',
-  favicon: 'img/favicon.ico',
+  title: "SpotX",
+  tagline: "A Multi-Purpose Ad-Blocker",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://SpotX-Official.github.io',
+  url: "https://SpotX-Official.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/spotx-docs',
+  baseUrl: "/spotx-docs",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'SpotX-Official', // Usually your GitHub org/user name.
-  projectName: 'spotx-docs', // Usually your repo name.
-  deploymentBranch:"gh-pages",
+  organizationName: "SpotX-Official", // Usually your GitHub org/user name.
+  projectName: "spotx-docs", // Usually your repo name.
+  deploymentBranch: "gh-pages",
   trailingSlash: false,
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
-          editUrl:
-            'https://github.com/SpotX-Official/spotx-docs/edit/main/',
+          // id: 'product', // omitted => default instance
+          path: "guide",
+          routeBasePath: "guide",
+          sidebarPath: "./sidebars.ts",
+          // ... other options
         },
-        // blog: {
-        //   showReadingTime: true,
-        //   editUrl:
-        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        // },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      } satisfies Preset.Options,
+      },
+    ],
+  ],
+  plugins: [
+    [
+      "@docusaurus/plugin-content-docs",
+      {
+        id: "faq",
+        path: "faq",
+        routeBasePath: "faq",
+        sidebarPath: "./sidebars.ts",
+        // ... other options
+      },
     ],
   ],
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/text_logo.png',
+    image: "img/text_logo.png",
     navbar: {
-      title: 'SpotX',
+      title: "SpotX",
       logo: {
-        alt: 'SpotX logo',
-        src: 'img/logo.png',
+        alt: "SpotX logo",
+        src: "img/logo.png",
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'guideSidebar',
-          position: 'left',
-          label: 'Guide',
+          type: "docSidebar",
+          sidebarId: "guideSidebar",
+          position: "left",
+          label: "Guide",
+        },
+        {
+          type: "docSidebar",
+          sidebarId: "faqSidebar",
+          position: "left",
+          label: "FAQ",
+          docsPluginId:"faq"
         },
         // {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/SpotX-Official',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/SpotX-Official",
+          label: "GitHub",
+          position: "right",
         },
       ],
-      
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Quick Start',
-              to: '/docs/quickstart',
+              label: "Quick Start",
+              to: "/docs/quickstart",
             },
             {
-              label: 'Guide',
-              to: '/docs/guide/hello',
+              label: "Guide",
+              to: "/guide/guide/hello",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/docusaurus",
             },
             {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus",
             },
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
+              label: "Twitter",
+              href: "https://twitter.com/docusaurus",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             // {
             //   label: 'Blog',
             //   to: '/blog',
             // },
             {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              label: "GitHub",
+              href: "https://github.com/facebook/docusaurus",
             },
           ],
         },
@@ -129,7 +141,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
-      additionalLanguages:['batch', 'powershell']
+      additionalLanguages: ["batch", "powershell"],
     },
   } satisfies Preset.ThemeConfig,
 };
---
+++ faq/introduction.md
@@ -0,0 +1,13 @@
+---
+sidebar_position: 1
+sidebar_label: Quick Start
+
+id: quickstart
+# slug: /quickstart  # not neccessary
+title: Quick Start
+tags:
+  - Getting Started
+  - Quick Start
+---
+
+# Welcome to FAQ
