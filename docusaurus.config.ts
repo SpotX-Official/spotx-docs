efe07cdf78a7bb7768612ccc278ad603936e6314
shinjith-dev <shinjithpallikkara@gmail.com>
2024-01-26 20:01:45 +0530

    fix: Fix Page overflow in chrome
    - Fixed page overflow in chrome browser
    - set defaul mode to dark

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -66,6 +66,9 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     image: "img/text_logo.png",
+    colorMode: {
+      defaultMode: "dark",
+    },
     navbar: {
       title: "SpotX",
       logo: {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,8 +8,8 @@ export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout description="A versatile ad-blocker designed for the Spotify desktop application on Windows, Linux, and Mac platforms">
-      <div className="home-bg h-screen w-screen absolute -z-10" />
-      <div className="home slate-100 w-screen min-h-screen relative">
+      <div className="home-bg h-full w-screen absolute -z-10" />
+      <div className="home slate-100 w-full min-h-screen relative">
         <div className="max-w-5xl mx-auto w-full space-y-8 py-8">
           <Header />
         </div>
